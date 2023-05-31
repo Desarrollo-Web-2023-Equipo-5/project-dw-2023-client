6c7297323ea36ef488ef187f7cf17e6a41072023
RODRIGO LUQUE <rodrigoluquepuig@gmail.com>
2023-05-30 22:18:05 -0300

    profile changes

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -6,6 +6,9 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { FeedComponent } from './pages/feed/feed.component';
 import { PostComponent } from './components/post/post.component';
 import { PostListComponent } from './components/post-list/post-list.component';
+import { MaterialModule } from '../shared/material/material.module';
+import { BottomNavComponent } from './components/bottom-nav/bottom-nav.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,9 @@ import { PostListComponent } from './components/post-list/post-list.component';
     FeedComponent,
     PostComponent,
     PostListComponent,
+    BottomNavComponent,
+    MainComponent,
   ],
-  imports: [CommonModule, ProtectedRoutingModule],
+  imports: [CommonModule, ProtectedRoutingModule, MaterialModule],
 })
 export class ProtectedModule {}
