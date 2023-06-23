8d7a798fb2732f6b8454c8b3daf73fb82ebd2f3c
RODRIGO LUQUE <rodrigoluquepuig@gmail.com>
2023-06-23 14:51:09 -0300

    added token renewal logic and session persistance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { validateTokenGuard } from './guards/validate-token.guard';
 
 const routes: Routes = [
   {
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
+    canActivate: [validateTokenGuard],
+    canActivateChild: [validateTokenGuard],
+    canMatch: [validateTokenGuard],
     loadChildren: () =>
       import('./protected/protected.module').then(m => m.ProtectedModule),
   },
