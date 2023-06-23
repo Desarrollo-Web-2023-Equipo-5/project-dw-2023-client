fbebb6442c5c33475e15cc62136534a41edad05a
RODRIGO LUQUE <rodrigoluquepuig@gmail.com>
2023-06-23 14:51:09 -0300

    added token renewal logic and session persistance

--- src/app/guards/validate-token.guard.ts
+++ src/app/guards/validate-token.guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { tap } from 'rxjs';
 
 export const validateTokenGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  return authService.validateToken().pipe(
+    tap(valid => {
+      if (!valid) {
+        router.navigateByUrl('/auth');
+      }
+    })
+  );
 };
