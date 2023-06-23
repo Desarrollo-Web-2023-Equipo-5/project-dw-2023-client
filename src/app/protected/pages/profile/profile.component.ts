90851da0a3beb6c1486f07719121b5458d097163
RODRIGO LUQUE <rodrigoluquepuig@gmail.com>
2023-06-23 20:19:16 -0300

    services and forms

--- src/app/protected/pages/profile/profile.component.ts
+++ src/app/protected/pages/profile/profile.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { User } from '../../../interfaces/user';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { ApiService } from '../../../services/api.service';
+import { mergeMap } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  user!: User;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private apiService: ApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(mergeMap(params => this.apiService.getUserById(params['id'])))
+      .subscribe({
+        next: res => {
+          this.user = res.user as User;
+        },
+      });
+  }
+}
