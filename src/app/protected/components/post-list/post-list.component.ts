210955ef683ac3d85d5b5089ccbd3b63f1274401
Ikken9 <pierosaucedo3@gmail.com>
2023-06-24 15:45:12 -0300

    Feed improvements

--- src/app/protected/components/post-list/post-list.component.ts
+++ src/app/protected/components/post-list/post-list.component.ts
@@ -1,5 +1,4 @@
 import {Component, Input} from '@angular/core';
-import { PostService } from "../../../services/post-service";
 import { IPost } from "../../../interfaces/post.interface";
 
 @Component({
@@ -9,59 +8,6 @@ import { IPost } from "../../../interfaces/post.interface";
 })
 export class PostListComponent {
 
-  @Input() category?: string;
-
-  posts: IPost[] = [
-    {
-      _id: 1,
-      creator: "AdventurerX",
-      title: "Chronicles of Eternia",
-      message: "Embark on a thrilling adventure through the enchanted realm of Eternia. " +
-        "Uncover ancient mysteries, battle fearsome creatures, and unlock the secrets of a " +
-        "world on the brink of darkness. Your destiny awaits in the Chronicles of Eternia, " +
-        "where heroes rise and legends are born " +
-        "Embark on a thrilling adventure through the enchanted realm of Eternia. " +
-        "Uncover ancient mysteries, battle fearsome creatures, and unlock the secrets of a " +
-        "world on the brink of darkness. Your destiny awaits in the Chronicles of Eternia, " +
-        "where heroes rise and legends are born" +
-        "Embark on a thrilling adventure through the enchanted realm of Eternia. " +
-        "Uncover ancient mysteries, battle fearsome creatures, and unlock the secrets of a " +
-        "world on the brink of darkness. Your destiny awaits in the Chronicles of Eternia, " +
-        "where heroes rise and legends are born" +
-        "Embark on a thrilling adventure through the enchanted realm of Eternia. " +
-        "Uncover ancient mysteries, battle fearsome creatures, and unlock the secrets of a " +
-        "world on the brink of darkness. Your destiny awaits in the Chronicles of Eternia, " +
-        "where heroes rise and legends are born.",
-      createdAt: 1,
-      updatedAt: 1,
-      deleted: false,
-      category: "campaigns"
-    }
-  ]
-  filteredPosts: IPost[] = []
-
-  constructor(private postService: PostService) {}
-
-  /**
-  ngOnInit(): void {
-    this.getPosts()
-  }
-   **/
-
-  ngOnChanges() {
-    this.filterPosts();
-  }
-
-  getPosts(): void {
-    this.postService.getAllPosts().subscribe(posts => this.posts = posts);
-  }
-
-  private filterPosts() {
-    if (this.category) {
-      this.filteredPosts = this.posts.filter(post => post.category === this.category);
-    } else {
-      this.filteredPosts = this.posts;
-    }
-  }
+  @Input() filteredPosts: IPost[] = [];
 
 }
--- src/app/services/post-service.ts
+++ src/app/services/post-service.ts
@@ -13,22 +13,26 @@ export class PostService {
   constructor(private httpClient: HttpClient) { }
 
   public getAllPosts(): Observable<any> {
-    return this.httpClient.get(`${this.apiUrl}/posts`)
+    return this.httpClient.get(`${this.apiUrl}/posts`);
+  }
+
+  public getPostsByParameter(category: string): Observable<any> {
+    return this.httpClient.get(`${this.apiUrl}?category=${category}`);
   }
 
   public getPostById(id: number): Observable<any> {
-    return this.httpClient.get(`${this.apiUrl}/posts/${id}`)
+    return this.httpClient.get(`${this.apiUrl}/posts/${id}`);
   }
 
   public createPost(req: Request) {
-    this.httpClient.post(`${this.apiUrl}/posts`, req)
+    this.httpClient.post(`${this.apiUrl}/posts`, req);
   }
 
   public updatePost(req: Request) {
-    this.httpClient.patch(`${this.apiUrl}/posts`, req)
+    this.httpClient.patch(`${this.apiUrl}/posts`, req);
   }
 
   public deletePost() {
-    this.httpClient.delete(`${this.apiUrl}/posts`)
+    this.httpClient.delete(`${this.apiUrl}/posts`);
   }
 }
